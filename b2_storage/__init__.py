409b553c6d0c7fdbb0f22d076bb113cec2b8a965
murchik <murchik@protonmail.com>
2017-09-03 21:19:05 +0800

    Stabilizing networking and more refactoring.

--- b2_storage/__init__.py
+++ b2_storage/__init__.py
@@ -2,4 +2,4 @@
 Django BlackBlaze B2 Storage
 """
 
-__version__ = (0, 3, 2)
+__version__ = (0, 3, 3)
--- b2_storage/storage.py
+++ b2_storage/storage.py
@@ -11,12 +11,14 @@ from .backblaze_b2 import BackBlazeB2
 
 @deconstructible
 class B2Storage(Storage):
+
     def __init__(self, account_id=None, app_key=None, bucket_name=None):
         overrides = locals()
         defaults = {
             'account_id': settings.BACKBLAZEB2_ACCOUNT_ID,
             'app_key': settings.BACKBLAZEB2_APP_KEY,
             'bucket_name': settings.BACKBLAZEB2_BUCKET_NAME,
+            'max_retries': settings.BACKBLAZEB2_MAX_RETRIES,
         }
         kwargs = {k: overrides[k] or v for k, v in defaults.items()}
         self.b2 = BackBlazeB2(**kwargs)
@@ -28,20 +30,15 @@ class B2Storage(Storage):
         """
 
         resp = self.b2.upload_file(name, content)
-        if 'fileName' in resp:
-            return resp['fileName']
-
-        else:
-            # Raise exception
-            pass
+        return resp['fileName']
 
     def exists(self, name):
         '''
-        BackBlaze B2 does not have a method to retrieve a filename info.
-        To get the info you need to make a download request, it will request the whole body.
-        imagine a file of 1 GB to only get the file info.
-        you can also list all files in that directory in chunks of 1000 imagine a directory of 10000.
-        For now it will only request return False.
+        BackBlaze B2 does not have a method to retrieve file info by file name.
+        To get the info you need to make a download request, it will request the
+        whole body. Imagine a file of 1 GB to only get the file info. You can
+        also list all files in that directory in chunks of 1000 imagine a
+        directory of 10000. For now it will only request return False.
         '''
 
         return False
